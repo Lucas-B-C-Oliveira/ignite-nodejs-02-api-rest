5a25661f8d60b6ef30585b929d8d64f7564646ab
Lucas Oliveira <lucascoliveira819@gmail.com>
2023-06-02 20:39:08 -0400

    fix: server

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,7 @@ import { env } from './env'
 const port = Number(process.env.PORT) || 3000//|| env.PORT || 3000; 
 
 app
-  .listen({
-    port,
-  })
+  .listen({ port, host: '0.0.0.0' })
   .then((a) => {
     console.log('HTTP Server Running!')
     console.log('HTTP Server Running!', port)
